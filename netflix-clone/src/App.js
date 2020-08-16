5db50095cb2735b15ca1efcc83d0c39b992d925d
SumitAnglekar <sumitanglekar@gmail.com>
2020-08-16 05:27:50 -0400

    initial develpment: rows,api,header setup

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Treding" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+export default App;
---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "27f9e4dcda871b0777f6ba7b462be5ea";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchComedyMovies:`/discover/tv?api_key=${API_KEY}&with_networks=35`,
+    fetchHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_networks=27`,
+    fetchRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_networks=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
