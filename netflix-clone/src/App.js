1d3e5a8a9e726aa5693ffa2a2bd05fa2dc57ebcd
SumitAnglekar <sumitanglekar@gmail.com>
2020-08-16 23:59:39 -0400

    firebase setup, All set and app is live

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      {/* <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> */}
       <Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
